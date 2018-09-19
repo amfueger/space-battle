247e9cce61301481ec3048e200fea20d4867fcc0
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-19 17:33:00 -0500

    space ship game last commit

--- js/app.js
+++ js/app.js
@@ -5,31 +5,24 @@ console.log('space battle');
 //hull
 //firepower
 //accuracy
-
 //ACTIONS
 //fire lazer
 //
-
 //-----------ENEMY
-
 //alien ship class will consist of
 //hull between 3 and 6
 //firepower between 2 and 4
 //accuracy between .6 and .8
-
 //Game logic, player must attack first
 //if alien survives
 //alien attacks
 //if player survives
 //player attacks
-
 //if player destroys ship either attack next OR retreat
 //(prompt)!!!
 //if you retreat then GAME OVER
 //if you attack again NEW enemy generates
 //or if you are destroyed GAME OVER
-
-
 //PLAYER OBJECT BUILD
 // const alien = {
 // 	name: '',
@@ -43,118 +36,125 @@ console.log('space battle');
 // 		}
 // 	}
 // }
-
 const player = {
 	name: 'USS Space America',
 	hull: 20,
 	firepower: 5,
 	accuracy: 0.7,
-	attack(){
-		if(Math.random() < player.accuracy) {
+	attack() {
+		if (Math.random() < player.accuracy) {
 			game.currentAlien.hull -= this.firepower
 			console.log(`${player.name}'s ship has used Photon torpedoes! The alien ships hull is down to ${game.printAlienStats()}`)
 		} else {
 			return 'You missed!';
 		}
 	}
+	isDead() {}
 }
 // player.attack();
 // console.log(`${alien.hull} has been reduced from alien hull!`);
 //-------------------ALIEN STUFF
-
 // We need to come back to this to figure out why it is
 // a string. parseFloat may help with this.
 // let alienAccuracy = (Math.random() * (max - min) + min).toPrecision(1)
 // let alienName = shipNames[Math.floor(Math.random() * shipNames.length)]
 // let alienFirePower = Math.floor(Math.random() * 3) + 2
 // let alienHull = Math.floor(Math.random() * 4) +3
-
 class Alien {
-	constructor(name,hull,firepower,accuracy){
+	constructor(name, hull, firepower, accuracy) {
 		this.name = name;
 		this.hull = hull;
 		this.firepower = firepower;
 		this.accuracy = accuracy;
 	}
-	fireLazer(firepower){
+	fireLazer(firepower) {
 		console.log('pew pew')
 		return game.currentAlien.firepower -= player.hull
-
 	}
-	isDead(){
-		if(currentAlien.hull <= 0){
+	isDead() {
+		if (currentAlien.hull <= 0) {
+			deadAliens += 1
 			return true;
 		} else {
 			return false;
 		}
 	}
 }
-
 // const alien = new Alien('Mothership', alienHull, alienFirePower, alienAccuracy)
 const game = {
-	currentAlien: null, 
-	generateNewAlien(){
-		const shipNames = ["Mothership", "Light Fighter", "Heavy Fighter"]
-		const min = 0.6
-		const max = 0.8
-		const alienAccuracy = Math.floor(Math.random() * (max - min) + min).toPrecision(1)
-		const alienName = shipNames[Math.floor(Math.random() * shipNames.length)]
-		const alienFirePower = Math.floor(Math.random() * 3) + 2
-		const alienHull = Math.floor(Math.random() * 4) +3
-		const alien = new Alien(alienName, alienHull,alienFirePower,alienAccuracy);
-		this.currentAlien = alien;
-		// console.log("new alien spawned")
-		// player.attack()
-	},
-	printAlienStats(){
-		console.log(this.currentAlien.hull)
-		return this.currentAlien.hull
-	},
-	gameOverMan(){
-		if(player.isDead()){
-			console.log()
-			return true
+		currentAlien: null,
+		generateNewAlien() {
+			const shipNames = ["Mothership", "Light Fighter", "Heavy Fighter"]
+			const min = 0.6
+			const max = 0.8
+			const alienAccuracy = Math.floor(Math.random() * (max - min) + min).toPrecision(1)
+			const alienName = shipNames[Math.floor(Math.random() * shipNames.length)]
+			const alienFirePower = Math.floor(Math.random() * 3) + 2
+			const alienHull = Math.floor(Math.random() * 4) + 3
+			const alien = new Alien(alienName, alienHull, alienFirePower, alienAccuracy);
+			this.currentAlien = alien;
+			// console.log("new alien spawned")
+			// player.attack()
+		},
+		printAlienStats() {
+			console.log(this.currentAlien.hull)
+			return this.currentAlien.hull
+		},
+		gameOverMan() {
+			if (player.isDead()) {
+				console.log(`Game over`)
+				return true;
+			}
+		},
+		retreat() {
+			console.log('Coward huh? Say goodbye to the planet I guess...');
+			return gameOverMan();
+			//if (fightOrFlight 
+			// return true
+		},
+		fight() {
+			//we have an alive alien, that's done by initialize alien
 		}
-	}, 
-	retreat(){
-		console.log('Coward huh? Say goodbye to the planet I guess...')
-		// return true
-	},
-	fightOrFlight(){
-		if(this.currentAlien.isDead()){
-			let option = prompt('Would you like to "fight" or "retreat"?')
-			if(option.toLowerCase() == 'fight') {
-				this.generateNewAlien();
-			} else if (option.toLowerCase() == 'retreat') { 
-				this.retreat()
+		initializeAlien {
+			currentAlien.isDead() {
+				if (prompt().option && pileOfAliens < 6) {
+					//Assume that option has come first, so we know whether we are fight or flight.
+					//generate alien in here
+					generateNewAlien();
+					return fight();
+				} else(pileOfAliens() == true) {
+					return gameOverMan();
+				}
+			}
+			pileOfAliens() {
+				if (deadAliens === 6) {
+					console.log('It looks like they are all dead... way to go... murderer... how many orphans do you think you just... nevermind...')
+				}
+			}
+			prompt() {
+				let option = prompt('Would you like to "fight" or "retreat"?')
+				if (option.toLowerCase() == 'fight') {
+					return this.fight();
+				} else if (option.toLowerCase() == 'retreat') {}
+				return this.retreat()
 			}
 		}
-	}
-}
-// game.generateNewAlien()
-// game.printAlienStats();
-
-// console.log(game.printAlienStats())
-//build an array to capture dead aliens
-game.generateNewAlien() 
-player.attack();
-console.log(game.currentAlien.hull)
-
-
-
-
-// let currentAlien;
-
-// write a function that that will generate a new alien
-// use that function in the logic if the alien dies so
-// you will generate a new alien. We can use a for loop
-// to do this a maximum of 6 times.
-
-
-
-// generateNewAlien();
-// console.log(currentAlien)
-// console.log(alienHull)
-// console.log(alienFirePower)
-// console.log(alienAccuracy)
-// console.log(game.currentAlien)
+		// game.generateNewAlien()
+		// game.printAlienStats();
+		let deadAliens = 0;
+		// console.log(game.printAlienStats())
+		//build an array to capture dead aliens
+		game.generateNewAlien()
+		player.attack();
+		console.log(game.currentAlien.hull)
+		// let currentAlien;
+		// write a function that that will generate a new alien
+		// use that function in the logic if the alien dies so
+		// you will generate a new alien. We can use a for loop
+		// to do this a maximum of 6 times.
+		// generateNewAlien();
+		// console.log(currentAlien)
+		// console.log(alienHull)
+		// console.log(alienFirePower)
+		// console.log(alienAccuracy)
+		// console.log(game.currentAlien)
