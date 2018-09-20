23f2ff1a29fdc7827ebe3ab05f564507be7dfeac
Ashley <ashley@Jamess-MacBook-Air.local>
2018-09-19 23:54:28 -0500

    Final Space Game

--- js/app.js
+++ js/app.js
@@ -36,21 +36,6 @@ console.log('space battle');
 // 		}
 // 	}
 // }
-const player = {
-	name: 'USS Space America',
-	hull: 20,
-	firepower: 5,
-	accuracy: 0.7,
-	attack() {
-		if (Math.random() < player.accuracy) {
-			game.currentAlien.hull -= this.firepower
-			console.log(`${player.name}'s ship has used Photon torpedoes! The alien ships hull is down to ${game.printAlienStats()}`)
-		} else {
-			return 'You missed!';
-		}
-	}
-	isDead() {}
-}
 // player.attack();
 // console.log(`${alien.hull} has been reduced from alien hull!`);
 //-------------------ALIEN STUFF
@@ -60,20 +45,41 @@ const player = {
 // let alienName = shipNames[Math.floor(Math.random() * shipNames.length)]
 // let alienFirePower = Math.floor(Math.random() * 3) + 2
 // let alienHull = Math.floor(Math.random() * 4) +3
-class Alien {
+// a player is an instance of a ship. So instead of an alien class, we do a ship class. 
+
+
+
+//6 aliens in this game.
+
+class Ship {
 	constructor(name, hull, firepower, accuracy) {
 		this.name = name;
 		this.hull = hull;
 		this.firepower = firepower;
 		this.accuracy = accuracy;
 	}
-	fireLazer(firepower) {
+	//took firepower out of parameter because it's redundant. we;re firing a lazer with the firepower of the ship, so we're referencing the ships property which we set in the constructor. 
+	//ADDED target, because it can be either for alien OR player. Makes it modular. 
+	fireLazer(target) {
 		console.log('pew pew')
-		return game.currentAlien.firepower -= player.hull
+		//Incorporate accuracy into the fire laser method. 
+		// stop harcoding specific object in class method, FIX
+		if (Math.random() < this.accuracy) {
+			target.hull -= this.firepower;
+			console.log(`${target.name} took ${this.firepower} damage. Owie?`);
+			return true;
+		} else {
+			console.log('Missed!');
+			return false;
+		}
+		// Trying to use the ship's accuracy property to determine if the laser hits the target. If it does, then we subtract firepower from the target's hull. If not then we cl "missed!", then return
+		// reversed this, applying the ships firepower to the target's defenses. firepower of the ship that's firing is beind deducted frrom the hull strength of the defender, which is target. "this" (<--- it's anonymous!) is the shooter, target, victim
+		// this is mutating function! When we call it, it just changes, does not return. The idea is that you call something to gather data and then evaluate. This is not those, it's just an action, thus it does not need a return. Return is just giving back data, so if there is no data to give, no return. return; <--- will exit the function, technically it returns undefined. 
 	}
 	isDead() {
-		if (currentAlien.hull <= 0) {
-			deadAliens += 1
+		// replace target with this, because we want to know if THIS PARTICULAR SHIP is being targetted. 
+		//this class needs to not care about whether it's a player or an alien. 
+		if (this.hull <= 0) {
 			return true;
 		} else {
 			return false;
@@ -81,80 +87,128 @@ class Alien {
 	}
 }
 // const alien = new Alien('Mothership', alienHull, alienFirePower, alienAccuracy)
-const game = {
-		currentAlien: null,
-		generateNewAlien() {
-			const shipNames = ["Mothership", "Light Fighter", "Heavy Fighter"]
-			const min = 0.6
-			const max = 0.8
-			const alienAccuracy = Math.floor(Math.random() * (max - min) + min).toPrecision(1)
-			const alienName = shipNames[Math.floor(Math.random() * shipNames.length)]
-			const alienFirePower = Math.floor(Math.random() * 3) + 2
-			const alienHull = Math.floor(Math.random() * 4) + 3
-			const alien = new Alien(alienName, alienHull, alienFirePower, alienAccuracy);
-			this.currentAlien = alien;
-			// console.log("new alien spawned")
-			// player.attack()
-		},
-		printAlienStats() {
-			console.log(this.currentAlien.hull)
-			return this.currentAlien.hull
-		},
-		gameOverMan() {
-			if (player.isDead()) {
-				console.log(`Game over`)
-				return true;
-			}
-		},
-		retreat() {
-			console.log('Coward huh? Say goodbye to the planet I guess...');
-			return gameOverMan();
-			//if (fightOrFlight 
-			// return true
-		},
-		fight() {
-			//we have an alive alien, that's done by initialize alien
+class Game {
+	constructor(aliensNumber) {
+		this.aliens = [];
+		this.player = this.generatePlayer();
+		for (let i = 0; i < aliensNumber; i++) {
+			this.aliens.push(this.generateNewAlien());
+			//NOW, we have an array of aliens, that we will lo00ooo000oop through later!!!!!1
 		}
-		initializeAlien {
-			currentAlien.isDead() {
-				if (prompt().option && pileOfAliens < 6) {
-					//Assume that option has come first, so we know whether we are fight or flight.
-					//generate alien in here
-					generateNewAlien();
-					return fight();
-				} else(pileOfAliens() == true) {
-					return gameOverMan();
+	}
+	//this function creates an alien and then returns it
+	generateNewAlien() {
+		const shipNames = ["Mothership", "Light Fighter", "Heavy Fighter"]
+		const alienAccuracy = Math.floor(Math.random() * (8 - 6) + 6) / 10
+		const alienName = shipNames[Math.floor(Math.random() * shipNames.length)]
+		const alienFirePower = Math.floor(Math.random() * 3) + 2
+		const alienHull = Math.floor(Math.random() * 4) + 3
+		const alien = new Ship(alienName, alienHull, alienFirePower, alienAccuracy);
+		return alien;
+		// console.log("new alien spawned")
+		// player.attack()
+	}
+	generatePlayer() {
+		//hull firepower, accuracy, name passing in arguments that the spec defined.
+		const name = 'USS Space America';
+		const hull = 20;
+		const firepower = 5;
+		const accuracy = .7;
+		//these things above, just got passed into the new player, that has a class of Ship!!!!!1
+		let player = new Ship(name, hull, firepower, accuracy);
+		return player;
+	}
+	//call initialize player, call intialize alien, player attacks, shit happens to alien, alien attacks, shit happens to player, the prompt pops up, takes input, moves on in the loop based on that, once alien or player isDead calls gameOverMan
+	//we need to return outcome of the battle. 
+	doBattle(player, alien) {
+		//call the prompt, decide to retreat or stay
+		while (true) {
+			//while will run continuously until it reaches an end state, which is one of three types of losses. 
+			this.printStats(player);
+			this.printStats(alien);
+			//this crap is here, because it's always printed out before the battle loop. Always. Forever. Infinite. Until someone dies.
+			if (this.getPlayerInput() == 'fight') {
+				//player attacks alien - needs to return damage, fire laser. It just targets alien, because the firelaser parameter passes in a ship. So we don't need to add hull, or anything like that, because it's already effing there. 
+				player.fireLazer(alien);
+				//player is firing laser at alien, that's what this means, omg. I can't even. Did you know that? I kinda did. Now I think I might know. I have to think of stuff doing stuff to stuff by hugging it. 
+				if (alien.isDead()) {
+					//player won that round. Just the battle that's over. 
+					console.log(`You killed the ${alien.name}, you mean old space-biddy`);
+					return "shits (alien) dead yo";
 				}
-			}
-			pileOfAliens() {
-				if (deadAliens === 6) {
-					console.log('It looks like they are all dead... way to go... murderer... how many orphans do you think you just... nevermind...')
+				//if alien is alive
+				alien.fireLazer(player);
+				//what's the outcome of this shit? Who fuckin' knows? 
+				if (player.isDead()) {
+					console.log("u ded.");
+					return "u ded";
 				}
-			}
-			prompt() {
-				let option = prompt('Would you like to "fight" or "retreat"?')
-				if (option.toLowerCase() == 'fight') {
-					return this.fight();
-				} else if (option.toLowerCase() == 'retreat') {}
-				return this.retreat()
+			} else {
+				return "retreated";
+				//this is not a message to the player, it's a message to the computer. Even though it means nothing to the computer. Helps me know what this return value symbolizes. Could return whatever the heckin' heck I want, but this makes sense later when looking over the code. 
+				//three outcomes, alien dead, player dead or player retreated. 
 			}
 		}
-		// game.generateNewAlien()
-		// game.printAlienStats();
-		let deadAliens = 0;
-		// console.log(game.printAlienStats())
-		//build an array to capture dead aliens
-		game.generateNewAlien()
-		player.attack();
-		console.log(game.currentAlien.hull)
-		// let currentAlien;
-		// write a function that that will generate a new alien
-		// use that function in the logic if the alien dies so
-		// you will generate a new alien. We can use a for loop
-		// to do this a maximum of 6 times.
-		// generateNewAlien();
-		// console.log(currentAlien)
-		// console.log(alienHull)
-		// console.log(alienFirePower)
-		// console.log(alienAccuracy)
-		// console.log(game.currentAlien)
+	}
+	gameLoop() {
+		//pick out the alien for the player to fight
+		for (let i = 0; i < this.aliens.length; i++) {
+			let outcome = this.doBattle(this.player, this.aliens[i]);
+			//save the results of the first call to do battle taking the return value and saving to a variable 
+			// Leaving this blank because nothing needs to happen. 
+			if (outcome == "shits (alien) dead yo") {
+				//check to see if there are more aliens alive
+			} else if (outcome == "u ded") {
+				return this.gameOverMan();
+			} else {
+				return this.retreat();
+			}
+			//the method took two arguments, and here they are. Now I deal with three different possible outcomes.  
+		} // if the player killed the first alien, we need to not call game over. 
+		//the alien outcome is different, because there could be more than one alien ship
+		return this.youIsWinner();
+	}
+	youIsWinner() {
+		console.log("Congrats! You have killed all the aliens!!!");
+		return true;
+	}
+	printStats(ship) {
+		console.log(`The ${ship.name} hull is ${ship.hull}`);
+	}
+	gameOverMan() {
+		console.log(`Game over`)
+		return true;
+	}
+	retreat() {
+		console.log('Coward huh? Say goodbye to the planet I guess...');
+		return true;
+		//if (fightOrFlight 
+		// return true
+	}
+	getPlayerInput() {
+		let option = prompt('Would you like to "fight" or "retreat"?')
+		if (option.toLowerCase() == 'fight' || option.toLowerCase() == 'retreat') {
+			return option;
+		} else {
+			return this.getPlayerInput();
+		}
+	}
+}
+
+// CALL THE GAME
+let game = new Game(6);
+game.gameLoop();
+//The game is running the game loop, passing 6 times for 6 aliens. aliensNumber. 
+
+
+// let currentAlien;
+// write a function that that will generate a new alien
+// use that function in the logic if the alien dies so
+// you will generate a new alien. We can use a for loop
+// to do this a maximum of 6 times.
+// generateNewAlien();
+// console.log(currentAlien)
+// console.log(alienHull)
+// console.log(alienFirePower)
+// console.log(alienAccuracy)
+// console.log(game.currentAlien)
